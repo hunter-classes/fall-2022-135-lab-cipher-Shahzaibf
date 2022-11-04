351a381e711b19f76bc3c24fbec023f7d387ace8
Shahzaib Fareed <shahzaibfareed1118@gmail.com>
2022-11-03 21:07:58 -0400

    Lab completed.

--- Makefile
+++ Makefile
@@ -4,13 +4,12 @@ main: main.o funcs.o
 tests: tests.o funcs.o
 	g++ -o tests tests.o funcs.o
 
-
-
 funcs.o: funcs.cpp funcs.h
 
 main.o: main.cpp funcs.h
 
 tests.o: tests.cpp doctest.h funcs.h
+	g++ -c -std=c++11 tests.cpp
 
 clean:
-	rm -f main.o funcs.o tests.o
+	rm -f *.o main tests funcs 
--- funcs.cpp
+++ funcs.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <vector>
+#include <cctype>
+#include <cmath>
+#include <cfloat>
 #include "funcs.h"
 
 // add functions here
+double distanceformula(double arr[26], double stagnant[26]) {
+	double total = 0;
+	for (int i = 0;i < 26;i++) {
+		total += pow((arr[i]-stagnant[i]),2);
+	}
+	return sqrt(total);
+}
 
 char shiftChar(char c, int rshift){
     int shiftedc = c + rshift;
@@ -78,7 +89,38 @@ std::string encryptVigenere(std::string plaintext, std::string keyword) {
       }
     return result;
 }
-
+void helper(double instances[][26], std::string input) {
+	for (int i = 0; i < 26; i++) {
+		int count = 0;
+		std::string test = encryptCaesar(input,i);
+		for (char c: test) {
+			if ((!isspace(c))&&(isalpha(c))) {
+				instances[i][tolower(c)-'a']++;
+				count++;
+			} 
+            }
+		for (int j = 0; j < 26; j++) {
+			instances[i][j]=instances[i][j]/count;
+		}
+	}
+}
 std::string solve(std::string encrypted_string) {
-    
+	double freq[26]={0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228, 0.02015, 0.06094, 0.06966, 0.00153, 0.00772, 0.04025, 0.02406, 0.06749, 0.07507, 0.01929, 0.00095, 0.05987, 0.06327, 0.09056, 0.02758, 0.00978, 0.02360, 0.00150, 0.01974, 0.00074};
+	double arr[26][26];
+	for (int a = 0; a <26; a++){
+        for (int b = 0; b < 26; b++){
+            arr[a][b]=0;
+        }
+    }	
+	helper(arr, encrypted_string);
+	double min = distanceformula(arr[0],freq);
+	int rot = 0;
+	for (int i = 1; i < 26; i++) {
+		double result = distanceformula(arr[i],freq);
+		if (min > result) {
+			min = result;
+			rot = i;
+		}
+	}
+	return encryptCaesar(encrypted_string,rot);
 }
